5a82bbb148670f5922f5035b930f28ce34eb8141
delancyma <xma52@jhu.edu>
2023-02-12 17:55:05 -0500

    pet

---
+++ src/pages/app/pet_peeves.tsx
@@ -0,0 +1,30 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import { api } from "../../utils/api";
+
+const login: NextPage = () => {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <main className="">
+      
+      <textarea className="textarea textarea_bordered w-96 h-32" placeholder="Interests"></textarea>
+      <div></div>
+      <textarea className="textarea textarea_bordered w-96 h-32" placeholder="Hobbies"></textarea>
+      <div></div>
+      <textarea className="textarea textarea_bordered w-96 h-32" placeholder="Background"></textarea>
+      <div></div>
+      <textarea placeholder="Anything about me!" className="textarea textarea-bordered textarea-lg w-full max-w-xs" ></textarea>
+      <div></div>
+      
+      <button className="btn btn-primary">Home</button>
+      </main>
+    </>
+  );
+};
+
+export default login;

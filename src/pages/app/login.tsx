acf5ba433726584343cab8fd2b85e247ce7e19cf
Johnnypham12373 <58641460+Johnnypham12373@users.noreply.github.com>
2023-02-12 14:34:21 -0500

    Push

--- src/pages/app/login.tsx
+++ src/pages/app/login.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import { api } from "../utils/api";
+import { api } from "../../utils/api";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="app/login"
+              href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Documentation →</h3>

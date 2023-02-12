824c640c4070e39b2b45cc1259f1ee50dd358aac
zyang101 <johnsonyangzjy@gmail.com>
2023-02-12 18:16:14 -0500

    a

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -13,7 +13,7 @@ const login: NextPage = () => {
       <main className="">
       <div></div>
       <div className="card h-200 w-96 bg-base-100 shadow-xl">
-  <figure><img src="Adam.jpg" alt= "Eric" /></figure>
+  <figure><img src="https://images.squarespace-cdn.com/content/v1/58c846642994ca3a008a6034/1559576853555-MFC8ONFCO2TNY5W6XISB/nile-scott-shots-boston-headshot-photography-portrait-12-of-12_1_orig.jpg?format=500w" alt= "Eric" /></figure>
   <div className="card-body">
     <h2 className="card-title">Eric Chiddleton</h2>
     <p> University: JHU</p>

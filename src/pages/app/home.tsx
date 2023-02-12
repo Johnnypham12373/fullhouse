d35d38177ce9f8e631e0125bd4202c24656047ab
zyang101 <johnsonyangzjy@gmail.com>
2023-02-12 17:12:25 -0500

    za

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -12,30 +12,13 @@ const login: NextPage = () => {
     <>
       <main className="">
       <div className="card h-200 w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <figure><img src="/images/Adam.jpg" /></figure>
   <div className="card-body">
     <h2 className="card-title">Eric</h2>
     <p> University: JHU</p>
-    <div className=""></div>
     <p> Grade: Freshman</p>
-    <div className=""></div>
     <div className="card-actions justify-end">
-    {/* The button to open modal */}
-    <label htmlFor="my-modal-5" className="btn">open modal</label>
-
-    {/* Put this part before </body> tag */}
-    <input type="checkbox" id="my-modal-5" className="modal-toggle" />
-    <div className="modal">
-    <div className="modal-box h-7/8 w-11/12 max-w-5xl">
-    <h3 className="font-bold text-lg">About me!</h3>
-    <p className="py-4">!</p>
-    <div className="modal-action">
-      <label htmlFor="my-modal-5" className="btn"></label>
-    </div>
-  </div>
-</div>
     <button className="btn btn-primary">Match!</button>
-
     <button className="btn btn-primary">Next</button>
     </div>
   </div>
@@ -48,4 +31,5 @@ const login: NextPage = () => {
 };
 
 export default login;
+
 

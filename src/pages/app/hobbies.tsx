8d413a58953898287e480373c97823dbdb7499a2
delancyma <xma52@jhu.edu>
2023-02-12 18:30:34 -0500

    d

--- src/pages/app/hobbies.tsx
+++ src/pages/app/hobbies.tsx
@@ -12,19 +12,21 @@ const login: NextPage = () => {
     <>
       <main className="">
       
-      <textarea className="textarea textarea_bordered w-96 h-35" placeholder="Interests"></textarea>
+      <textarea className="textarea textarea_bordered w-96 h-37" placeholder="Interests"></textarea>
       <div></div>
-      <textarea className="textarea textarea_bordered w-96 h-35" placeholder="Hobbies"></textarea>
+      <textarea className="textarea textarea_bordered w-96 h-37" placeholder="Hobbies"></textarea>
       <div></div>
-      <textarea className="textarea textarea_bordered w-96 h-35" placeholder="Background"></textarea>
+      <textarea className="textarea textarea_bordered w-96 h-37" placeholder="Background"></textarea>
       <div></div>
       <textarea placeholder="Anything about me!" className="textarea textarea-bordered textarea-lg w-full max-w-xs" ></textarea>
       <div></div>
       
-      <Link href = "/preferences" className="form-control mt-6">
-        <button className="btn btn-sm">Back</button>
+      <Link href = "preferences" className="form-control mt-6">
+        <div className='fixed bottom-0 w-full'>
+            <button className="bottom-0 btn btn-sm">Back</button>
+        </div>
       </Link>
-      <Link href = "/preferences" className="form-control mt-6">
+      <Link href = "preferences" className="form-control mt-6">
         <button className="btn btn-sm">Next</button>
       </Link>
     
